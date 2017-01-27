7a104c9bc97abad0814ba53ebefee8701eb23fe8
Sruti Guhathakurta <sruti@gatech.edu>
2017-01-27 14:06:09 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+from app import views
+app.secret_key= "secretKey1"
+
---
+++ app/views.py
@@ -0,0 +1,6 @@
+from app import app
+from flask import render_template, request, url_for, redirect, flash, session
+
+@app.route('/')
+def index():
+    return render_template('index.html')
