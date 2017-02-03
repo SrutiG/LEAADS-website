fd8c8ef40f9c975d988f9c6b70fef947a3e0e6ce
Sruti Guhathakurta <sruti@gatech.edu>
2017-02-02 20:27:08 -0500

    got rid of unnecessary stuff

--- app/views.py
+++ app/views.py
@@ -4,3 +4,4 @@ from flask import render_template, request, url_for, redirect, flash, session
 @app.route('/')
 def index():
     return render_template('index.html')
+
--- run.py
+++ run.py
@@ -3,7 +3,4 @@ import os
 app.debug = True
 
 if  __name__ == "__main__":
-    app.run(
-        host=os.getenv('LISTEN','0.0.0.0'),
-        port=int(os.getenv('PORT', '8080'))
-    )
+    app.run()
