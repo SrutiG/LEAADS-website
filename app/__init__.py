51ea7b165567bfd4c751881aef08b9abc079a766
Sruti Guhathakurta <sruti@gatech.edu>
2017-02-09 19:39:52 -0500

    changes..

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
-#from flask_mysqldb import MySQL
-=======
->>>>>>> fd8c8ef40f9c975d988f9c6b70fef947a3e0e6ce
 
 app = Flask(__name__)
 from app import views
--- run.py
+++ run.py
@@ -3,10 +3,4 @@ import os
 app.debug = True
 
 if  __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(
-        
-    )
-=======
     app.run()
->>>>>>> fd8c8ef40f9c975d988f9c6b70fef947a3e0e6ce
