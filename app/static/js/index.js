63da103acaa3406a9c6c57632a7b780913985b1e
Sruti Guhathakurta <sruti@gatech.edu>
2017-02-10 16:18:19 -0500

    added active class to navbar

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+    $home_link = $("#home-link");
     $('#nav').affix({
         offset: {
             top: $('#nav').offset().top
         }
     });
+    if (window.location.pathname == '/home') {
+        $home_link.addClass('active');
+    } else {
+        $home_link.removeClass('active');
+    }
+
 })
