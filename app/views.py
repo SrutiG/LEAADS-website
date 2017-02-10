8ba41a206b9f76f10248f9bc5cd223d1cc6243a2
Sruti Guhathakurta <sruti@gatech.edu>
2017-02-10 01:08:30 -0500

    added a header and a basic navbar, random pic for now

--- app/views.py
+++ app/views.py
@@ -5,3 +5,6 @@ from flask import render_template, request, url_for, redirect, flash, session
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
