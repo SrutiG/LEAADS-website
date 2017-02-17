e49ec5ae81e3ff9fff19e3b3d1138765a7239ab8
Sruti Guhathakurta <sruti@gatech.edu>
2017-02-16 23:18:40 -0500

    active nav-links

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $home_link = $("#home-link");
+    $opp_link = $("#opp-link");
+    $about_link = $("#about-link");
     $('#nav').affix({
         offset: {
             top: $('#nav').offset().top
@@ -11,6 +13,18 @@ $(document).ready(function() {
         $home_link.addClass('active');
     } else {
         $home_link.removeClass('active');
+    }
+
+    if (window.location.pathname == '/opportunities_list') {
+        $opp_link.addClass('active');
+    } else {
+        $opp_link.removeClass('active');
+    }
+
+    if (window.location.pathname == '/about_us') {
+        $about_link.addClass('active');
+    } else {
+        $about_link.removeClass('active');
     }
 
 })
