652f74709f0ed1ac6877b501195ba371f21588b6
Sruti Guhathakurta <sruti@gatech.edu>
2017-04-05 22:24:41 -0400

    active link changes based on page

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $admin_dash = $("#admin-dash");
     $admin_home = $("#admin-home");
+    $admin_blog = $("#admin-blog");
+    $admin_opp = $("#admin-opp");
+    $admin_prog = $("#admin-prog");
+    $admin_members = $("#admin-members");
     if (window.location.pathname == '/admin_dashboard') {
             $admin_dash.addClass('active-cust');
     } else {
@@ -11,5 +15,29 @@ $(document).ready(function() {
         $admin_home.addClass('active-cust');
     } else {
         $admin_home.removeClass('active-cust');
+    }
+
+    if (window.location.pathname == '/admin_blog') {
+        $admin_blog.addClass('active-cust');
+    } else {
+        $admin_blog.removeClass('active-cust');
+    }
+
+    if (window.location.pathname == '/admin_opp') {
+        $admin_opp.addClass('active-cust');
+    } else {
+        $admin_opp.removeClass('active-cust');
+    }
+
+    if (window.location.pathname == '/admin_prog') {
+        $admin_prog.addClass('active-cust');
+    } else {
+        $admin_prog.removeClass('active-cust');
+    }
+
+    if (window.location.pathname == '/admin_members') {
+        $admin_members.addClass('active-cust');
+    } else {
+        $admin_members.removeClass('active-cust');
     }
 })
